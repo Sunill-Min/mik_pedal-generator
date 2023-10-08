2988faa8fceb3e8bbe81c9ef5c8036a7c33dabcb
sunill_gram\sunill <min.sunill09@gmail.com>
2023-10-09 00:36:28 +0900

    arduino Ver1.0.0

---
+++ pedal-generator-arduino/pedal-generator-arduino.ino
@@ -0,0 +1,178 @@
+// 와이파이모듈 라이브러리
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+// 전류센서모듈 라이브러리
+#include <Wire.h>
+#include <INA219_WE.h>
+
+// 상수 설정
+#ifndef STASSID
+#define STASSID "wifi5078" // 와이파이 이름
+#define STAPSK "19900720" // 와이파이 비밀번호
+
+#define I2C_ADDRESS 0x40 // I2C 주소
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+float shuntVoltage_mV = 0.0;
+float loadVoltage_V = 0.0;
+float busVoltage_V = 0.0;
+float current_mA = 0.0;
+float power_mW = 0.0;
+bool ina219_overflow = false;
+
+ESP8266WebServer server(80);
+
+INA219_WE ina219(I2C_ADDRESS);
+
+void handleRoot() {
+  server.sendHeader("Access-Control-Allow-Origin","*");
+  server.send(200, "Application/json", "{\"loadVoltage\":" + String(loadVoltage_V) + ", \"current\":" + String(current_mA) + "}");
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) { message += " " + server.argName(i) + ": " + server.arg(i) + "\n"; }
+  server.send(404, "text/plain", message);
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected WiFi Name : ");
+  Serial.println(ssid);
+  Serial.print("IP address : ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) { Serial.println("MDNS responder started"); }
+
+  server.on("/", handleRoot);
+
+  server.onNotFound(handleNotFound);
+
+  /////////////////////////////////////////////////////////
+  // Hook examples
+
+  server.addHook([](const String& method, const String& url, WiFiClient* client, ESP8266WebServer::ContentTypeFunction contentType) {
+    (void)method;       // GET, PUT, ...
+    (void)url;          // example: /root/myfile.html
+    (void)client;       // the webserver tcp client connection
+    (void)contentType;  // contentType(".html") => "text/html"
+    Serial.printf("A useless web hook has passed\n");
+    Serial.printf("(this hook is in 0x%08x area (401x=IRAM 402x=FLASH))\n", esp_get_program_counter());
+    return ESP8266WebServer::CLIENT_REQUEST_CAN_CONTINUE;
+  });
+
+  server.addHook([](const String&, const String& url, WiFiClient*, ESP8266WebServer::ContentTypeFunction) {
+    if (url.startsWith("/fail")) {
+      Serial.printf("An always failing web hook has been triggered\n");
+      return ESP8266WebServer::CLIENT_MUST_STOP;
+    }
+    return ESP8266WebServer::CLIENT_REQUEST_CAN_CONTINUE;
+  });
+
+  server.addHook([](const String&, const String& url, WiFiClient* client, ESP8266WebServer::ContentTypeFunction) {
+    if (url.startsWith("/dump")) {
+      Serial.printf("The dumper web hook is on the run\n");
+
+      // Here the request is not interpreted, so we cannot for sure
+      // swallow the exact amount matching the full request+content,
+      // hence the tcp connection cannot be handled anymore by the
+      // webserver.
+#ifdef STREAMSEND_API
+      // we are lucky
+      client->sendAll(Serial, 500);
+#else
+      auto last = millis();
+      while ((millis() - last) < 500) {
+        char buf[32];
+        size_t len = client->read((uint8_t*)buf, sizeof(buf));
+        if (len > 0) {
+          Serial.printf("(<%d> chars)", (int)len);
+          Serial.write(buf, len);
+          last = millis();
+        }
+      }
+#endif
+      // Two choices: return MUST STOP and webserver will close it
+      //                       (we already have the example with '/fail' hook)
+      // or                  IS GIVEN and webserver will forget it
+      // trying with IS GIVEN and storing it on a dumb WiFiClient.
+      // check the client connection: it should not immediately be closed
+      // (make another '/dump' one to close the first)
+      Serial.printf("\nTelling server to forget this connection\n");
+      static WiFiClient forgetme = *client;  // stop previous one if present and transfer client refcounter
+      return ESP8266WebServer::CLIENT_IS_GIVEN;
+    }
+    return ESP8266WebServer::CLIENT_REQUEST_CAN_CONTINUE;
+  });
+
+  // Hook examples
+  /////////////////////////////////////////////////////////
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  Wire.begin();
+  if(!ina219.init()){
+    Serial.println("INA219 not connected!");
+  }
+  Serial.println("INA219 Current Sensor with solar panel");
+
+  Serial.println("Start Program---------------------\n\n\n");
+}
+
+void loop(void) {
+  server.handleClient();
+  MDNS.update();
+  // float shuntVoltage_mV = 0.0;
+  // float loadVoltage_V = 0.0;
+  // float busVoltage_V = 0.0;
+  // float current_mA = 0.0;
+  // float power_mW = 0.0; 
+  // bool ina219_overflow = false;
+  
+  shuntVoltage_mV = ina219.getShuntVoltage_mV();
+  busVoltage_V = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getBusPower();
+  loadVoltage_V  = busVoltage_V + (shuntVoltage_mV/1000);
+  ina219_overflow = ina219.getOverflow();
+  
+  // Serial.print("Shunt Voltage [mV]: "); Serial.println(shuntVoltage_mV);
+  // Serial.print("Bus Voltage [V]: "); Serial.println(busVoltage_V);
+  Serial.print("Load Voltage [V]: "); Serial.println(loadVoltage_V);
+  Serial.print("Current[mA]: "); Serial.println(current_mA);
+  // Serial.print("Bus Power [mW]: "); Serial.println(power_mW);
+
+  if(!ina219_overflow){
+    Serial.println("Values OK - no overflow");
+  }
+  else{
+    Serial.println("Overflow! Choose higher PGAIN");
+  }
+  Serial.println();
+
+  delay(1000);
+}
